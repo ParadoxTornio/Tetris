30e3265eedce0efa0617fdb31109a294245d17f1
paradox <paradox.tornio@gmail.com>
2023-10-25 17:46:23 +0300

    v0.4

--- game.py
+++ game.py
@@ -23,12 +23,15 @@ class MyGame(arcade.Window):
         self.block4x1 = None
         self.block1x1 = None
         self.next_block = None
+        self.next_block_name = None
         self.next_block_image = None
         self.blocks_images = None
+        self.ground = None
 
         self.background = arcade.load_texture('images/background.png')
 
     def setup(self):
+        self.ground = arcade.Sprite('images/ground.png', center_x=176, center_y=500, hit_box_algorithm='Simple')
         self.blocks_list = arcade.SpriteList()
 
         self.block_1 = Block('block1', 8, 20)
@@ -40,24 +43,31 @@ class MyGame(arcade.Window):
         self.blocks_list.append(self.block_2)
         self.blocks_list.append(self.block2x2)
         self.blocks_list.append(self.block4x1)
-        self.blocks_images = ['images/block1.png',
-                              'images/block2.png',
-                              'images/block4x1.png',
-                              'images/block2x2.png']
-        self.next_block = random.choice(self.blocks_images)
-        self.next_block_image = arcade.load_texture(str(self.next_block))
+        self.blocks_images = ['block1',
+                              'block2',
+                              'block4x1',
+                              'block2x2']
+        self.next_block_name = random.choice(self.blocks_images)
+        self.next_block_image = arcade.load_texture(f'images/{self.next_block_name}.png')
+        self.next_block = Block(self.next_block_name, 5, 2)
+        self.blocks_list.append(self.next_block)
 
-        self.physics_engine = arcade.PymunkPhysicsEngine((0, 0))
-        self.physics_engine.add_sprite_list(self.blocks_list, friction=0, damping=0)
+        self.physics_engine = arcade.PymunkPhysicsEngine((0, -1000))
+        self.physics_engine.add_sprite(self.ground, collision_type='wall', body_type=arcade.PymunkPhysicsEngine.STATIC,
+                                       friction=1)
+        self.physics_engine.add_sprite_list(self.blocks_list, friction=0, damping=0, collision_type='player')
 
     def on_draw(self):
         self.clear()
         arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.blocks_list.draw()
-        # for block in self.blocks_list:
-        #     block.draw_hit_box(color=arcade.color.RED)
-        print(self.next_block)
-        arcade.draw_lrwh_rectangle_textured(500, 400, SCREEN_WIDTH, SCREEN_HEIGHT, self.next_block_image)
+        for block in self.blocks_list:
+            block.draw_hit_box(color=arcade.color.RED)
+        # if self.next_block
+        arcade.draw_lrwh_rectangle_textured(375, 490, self.next_block_image.width // 2,
+                                            self.next_block_image.height // 2, self.next_block_image)
+        self.ground.draw()
+        self.ground.draw_hit_box(color=arcade.color.RED)
 
     def update_player_speed(self):
         pass
